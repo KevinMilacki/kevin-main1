9f78bafd43da847e3ae559ef48194077fb100df6
Kevin Milacki <kevin.milacki@gmail.com>
2023-12-03 12:48:30 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      
+    <div>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>KevinMilacki.com</h1>
+      <ul className="text-2xl font-bold underline border-2 rounded-lg p-10 text-center">
+      <li>Whats For Dinner App</li>
+      <li>Photos</li>
+      <li>Creative Writing</li>
+
+
+    </ul>
       </header>
+    </div>
     </div>
   );
 }
